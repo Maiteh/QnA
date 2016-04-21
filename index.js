5da3163411f990af7cb44fea5b14ec0fc6a23494
Maïteh Mertens <maiteh_m@hotmail.com>
2016-04-21 15:31:04 +0200

    added views

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require("express");
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+// choose a port
+var port = 3700;
+//get root sor just localhost:3700/
+app.get("/", function(req, res){
+  res.send('Connection etablished!');
+});
+//Tell the server to which port to listen
+server.listen(port);
+console.log('Server started on port ' + port);
+// declare where you define your views
+app.set('views', __dirname + '/views');
+//what view engine, ejs or jade
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
 
-//routing and
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get("/", function(req, res){
+  res.render("index");
 });
 
-//Listen to the connection event for incoming sockets, and I log it to the console
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+// Pass express to socket.io
+var io = require('socket.io').listen(server);
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+io.sockets.on('connection', function (socket) {
+
 });
