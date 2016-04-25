997fcb6d9872cb6677840f523f35bc6cce7c6b06
Maïteh Mertens <maiteh_m@hotmail.com>
2016-04-26 00:12:34 +0200

    Edited index.js and package.json. Added socket.io connection and mongodb connection

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var app = express();
 var server = http.createServer(app);
 // choose a port
 var port = 3700;
-//get root sor just localhost:3700/
+//Mongo connection to save the messages
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost:27017/q-a';
+var Schema = mongoose.Schema;
+var connection = mongoose.createConnection(url);
+
+//get root or just localhost:3700/
 app.get("/", function(req, res){
   res.send('Connection etablished!');
 });
@@ -21,9 +27,21 @@ app.get("/", function(req, res){
   res.render("index");
 });
 
-// Pass express to socket.io
+/**
+ * Pass express to socket.io
+ * This is where were alle the code belongs
+ * This method will handle all connection from all clients connected to our server.
+ * The object socket is actually socket of the client.
+ * This is the way client connect and send event to our server.
+ * To send message to client we only need to do this
+ * @type {http.Server}
+ */
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
 
+  //This will only send event to socket client
+  socket.emit('message', messages);
+  //broadcast to all clients
+  io.sockets.in(room_id).emit('message', message);
 });
