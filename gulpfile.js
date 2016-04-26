7dc023802bc371fd25df222560fcfd1b9d5886a4
Maïteh Mertens <maiteh_m@hotmail.com>
2016-04-26 12:41:14 +0200

    Edited package.json + gulpfile added minify and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp       = require('gulp');
 var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
 
+// Minify css for preformance
+gulp.task('minify-css', function() {
+    return gulp.src('assets/css/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('assets/styles'));
+});
+
+// Live reload of the browser.
 gulp.task('browser-sync', function() {
     browserSync.init({
         //injectChanges: true,
         host: 'localhost',
-        proxy: 'localhost:3700',
+        proxy: 'localhost:3700/'
     });
-    gulp.watch("assets/css/*.css").on('change', browserSync.reload);
+    // Call function gulp watch to check if changes
+    // if changes -> reload !!
+    gulp.watch("assets/css/*.css", ['minify-css']).on('change', browserSync.reload);
 });
 
 gulp.task('watch', function () {
-    gulp.watch('assets/css/*.css' ['browser-sync']);
+    gulp.watch('assets/css/*.css' ['minify-css']);
 });
