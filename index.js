c979a61ec95e8b87c42627000b063aaf2961d96b
Maïteh Mertens <maiteh_m@hotmail.com>
2016-05-03 12:32:15 +0200

    Edited index.js added message schema and update date

--- index.js
+++ index.js
@@ -18,6 +18,28 @@ var userSchema = new Schema({
     password: { type: String, required: true },
     created_at: Date,
     updated_at: Date
+})
+// Add the date before any save
+userSchema.pre('save', function(next) {
+    // get the current date
+    var currentDate = new Date();
+
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at)
+        this.created_at = currentDate;
+
+    next();
+});
+var messageSchema = new Schema({
+    message_id: Number,
+    user_id: String,
+    user_name: String,
+    room_id: String,
+    message: String,
+    created_at: Date
 });
 
 var server = app.listen(3000, function() {
