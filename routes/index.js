12e77843a325dddf9218e9749af394bf85972723
Maïteh Mertens <maiteh_m@hotmail.com>
2016-05-14 10:23:41 +0200

    Edited routes index.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+/**
+* Important is the passport.authenticate() to delegate 
+* the authentication to login and signup strategies when 
+* a HTTP POST is made to /login and /signup routes respectively. 
+* Note that it is not mandatory to name the strategies on the route path and 
+* it can be named anything.
+*/
+module.exports = function(passport){
+ 
+  /* GET login page. */
+  router.get('/', function(req, res) {
+    // Display the Login page with any flash message, if any
+    res.render('index', { message: req.flash('message') });
+  });
+ 
+  /* Handle Login POST */
+  router.post('/login', passport.authenticate('login', {
+    successRedirect: '/home',
+    failureRedirect: '/',
+    failureFlash : true 
+  }));
+ 
+  /* GET Registration Page */
+  router.get('/signup', function(req, res){
+    res.render('register',{message: req.flash('message')});
+  });
+ 
+  /* Handle Registration POST */
+  router.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/home',
+    failureRedirect: '/signup',
+    failureFlash : true 
+  }));
+ 
+  return router;
+}
