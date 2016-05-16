b976ead6841ad34a8a0bd721e2dd953a8bf440f1
Maïteh Mertens <maiteh_m@hotmail.com>
2016-05-16 22:24:00 +0200

    Updated gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,49 @@
-var gulp       = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var concatCSS = require('gulp-concat-css');
-var nodemon = require('gulp-nodemon');
-var notify = require('gulp-notify');
-var browserSync = require('browser-sync')
-var reload = browserSync.reload;
+var gulp        = require('gulp');
+var cleanCSS    = require('gulp-clean-css');
+var concatCSS   = require('gulp-concat-css');
+var nodemon     = require('gulp-nodemon');
+var notify      = require('gulp-notify');
+var sass        = require('gulp-sass');
+
+// nodemon
+gulp.task('start', function () {
+  nodemon({
+    script: 'app.js', 
+    ext: 'js html', 
+    env: { 
+        'NODE_ENV': 'development' 
+    }
+  })
+  .pipe(notify("App started successfully"));
+});
+
+// convert sass
+gulp.task('sass', function() {
+    gulp.src('public/stylesheets/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/stylesheets/css/'))
+});
 
 
 // Minify css for preformance
 gulp.task('minify-css', function() {
-    return gulp.src('assets/css/*.css')
+    return gulp.src('public/stylesheets/css/*.css')
         // Merge all the css files to one file.
-        .pipe(concatCSS('styles/bundle.css'))
+        .pipe(concatCSS('public/stylesheets/style.css'))
         // Minify css
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('assets/styles'));
+     .pipe(gulp.dest('public/stylesheets'));
 });
 
-gulp.task('browser-sync', ['nodemon'], function() {
-    browserSync({
-        proxy: "localhost:3000",  // local node app address
-        port: 5000,  // use *different* port than above
-        notify: true
-    });
+//Past all css files in to one
+gulp.task('concat', function () {
+  return gulp.src('public/stylesheets/css/*.css')
+    .pipe(concatCss("public/stylesheets/style.css"))
+    .pipe(gulp.dest('public/stylesheets'));
 });
 
-gulp.task('nodemon', function (cb) {
-    var called = false;
-    return nodemon({
-        script: 'index.js',
-        ignore: [
-            'gulpfile.js',
-            'node_modules/'
-        ]
-    })
-        .on('start', function () {
-            if (!called) {
-                called = true;
-                cb();
-            }
-        })
-        .on('restart', function () {
-            setTimeout(function () {
-                reload({ stream: false });
-            }, 1000);
-        });
-});
 
-gulp.task('default', ['browser-sync'], function () {
-    gulp.watch(['index.js'], reload);
+// Default
+gulp.task('default',['nodemon'],function() {
+    gulp.watch('./public/stylesheets/sass/*.scss',['sass', 'minify-css', 'concat']);
 });
